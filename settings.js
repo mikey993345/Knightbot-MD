88dc4930596d579692c8af868cf70da8e9572d46
mikey993345 <orgtrading682@gmail.com>
2025-03-13 14:31:40 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'mikey',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ondas",
+  botOwner: 'mikey', // Your name
+  ownerNumber: '9072178834', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
